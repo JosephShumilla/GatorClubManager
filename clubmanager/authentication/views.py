c6cd9fd090a4e2e410c1cb2b4c37ddb367ddcbd0
Nicco <nturillo@ufl.edu>
2024-10-24 14:57:00 -0400

    added authentication app

---
+++ clubmanager/authentication/views.py
@@ -0,0 +1,17 @@
+# views.py
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.urls import reverse_lazy
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()  # Save the new user to the database
+            login(request, user)  # Log the user in after sign-up
+            return redirect('')  # Redirect to home page or any other page
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'signup.html', {'form': form})
