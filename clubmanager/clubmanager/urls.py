bec0a15ec61ac43a93e9b44f91db3c81fe471461
Aln732 <alexneil732@gmail.com>
2024-11-04 11:11:59 -0500

    Updates on create a club and join club

--- clubmanager/clubmanager/urls.py
+++ clubmanager/clubmanager/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
     path('members/', include('django.contrib.auth.urls')),
     path('members/', include('members.urls')),
     path('createClub.html', views.createClub, name='createClub'),
-    path('joinClubs.html', views.joinClubs, name='joinClub'),
+    path('joinClubs.html', views.item_list, name='joinClub'),
     path('index.html', views.index, name='home'),
     path('myClubs.html', views.myClubs, name='myClubs'),
     path('upcomingEvents.html', views.upcomingEvents, name='upcomingEvents'),
--- clubmanager/members/views.py
+++ clubmanager/members/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+
 from .models import Club, Membership
+
+from django.core.paginator import Paginator
+
+
+
+def item_list(request):
+    items = Club.objects.all()  # Fetch all items from the database
+    paginator = Paginator(items, 10)  # Show 10 items per page
+
+    page_number = request.GET.get('page')  # Get the page number from the request
+    page_obj = paginator.get_page(page_number)  # Get the page of items
+
+    return render(request, 'authenticate/joinClubs.html', {'page_obj': page_obj})
 
 def login_user(request):
     return render(request, 'authenticate/Sign_In.html', {})   
