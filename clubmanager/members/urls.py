70a27b3e0e6d8c1622307cef704cee04565ded8f
JosephShumilla <shumillajd@hotmail.com>
2024-11-15 10:11:25 -0500

    Combining stuff

--- clubmanager/members/urls.py
+++ clubmanager/members/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from .views import joinClubs
 
 urlpatterns = [
     path('', views.first_view, name='first_view'),
     path('search/', views.searchClubs, name='search_clubs'),
+    path('create-club/', views.createClub, name='create_club'),
 ]
