c1e6dd03cd35bca73e9b1d1afcc934b522c07a3f
nwilkie22 <149012402+nwilkie22@users.noreply.github.com>
2024-11-19 16:35:54 -0500

    Added leave button to my clubs page

--- clubmanager/members/views.py
+++ clubmanager/members/views.py
@@ -146,7 +146,20 @@ def myClubs(request):
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
 
-    return render(request, 'main_sites/myClubs.html', {'page_obj': page_obj})
+    club_data = []
+    for item in page_obj.object_list:
+        if user.is_authenticated:
+            is_member = Membership.objects.filter(user=user, club=item).exists()
+        else:
+            is_member = False
+        club_data.append({'club': item, 'is_member': is_member})
+
+    context = {
+        'page_obj': page_obj,
+        'club_data': club_data
+    }
+
+    return render(request, 'main_sites/myClubs.html', context=context)
 
 def index(request):
     return render(request, 'main_sites/index.html',{})
